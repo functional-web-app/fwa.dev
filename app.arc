7106a826aaeb296f8cb9c772531539880f538fdc
Taylor Beseda <tbeseda@gmail.com>
2022-02-02 21:11:36 -0700

    trim down deps

--- app.arc
+++ app.arc
@@ -2,10 +2,6 @@
 fwa
 
 @http
-/blog/*
-  src src/blog
-  method any
-
 /*
   src src/default
   method any
--- src/blog/index.js
+++
@@ -1,5 +0,0 @@
-let arc = require('@architect/functions')
-let home = require('./home')
-let post = require('./post')
-
-exports.handler = arc.http.async(home, post)
