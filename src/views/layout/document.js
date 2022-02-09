aced6a877811b40bbb5dcbda46c5e7beb80b362f
Brian LeRoux <b@brian.io>
2022-02-09 09:54:40 -0800

    inlining stuff to make webpagetest better

--- src/views/layout/document.js
+++ src/views/layout/document.js
@@ -16,7 +16,26 @@ module.exports = function wrap (content) {
       "
     >
     ${content}
-    <script type="module" src="/_static/index.js"></script>
+    <script type="module">
+    /* inline from public */
+(function (){
+  const navToggle = document.getElementById('nav-toggle')
+  const navUL = document.getElementById('main-nav')
+
+  navToggle.addEventListener('click', () => {
+    navUL.classList.toggle('hidden')
+    navUL.classList.toggle('mobile-nav-open')
+  })
+
+  document.addEventListener('click', (e) => {
+    if (!navUL.classList.contains('hidden') && !navToggle.contains(e.target)) {
+      navUL.classList.add('hidden')
+      navUL.classList.remove('mobile-nav-open')
+    }
+  })
+})()
+
+    </script>
   </body>
 </html>
   `
